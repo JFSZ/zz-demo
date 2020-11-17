2af47d51f51f430ff3e6a9609319fd84b4143d16
843400447@qq.com <chenxue@2020>
2020-11-17 20:01:09 +0800

    2020-11-17 elasticsearch 模块创建

---
+++ elasticsearch-demo/src/test/java/com/example/elasticsearchdemo/ESApplication.java
@@ -0,0 +1,17 @@
+package com.example.elasticsearchdemo;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ *  * @Description: TODO
+ *  * @Author: chenxue
+ *  * @Date: 2020-11-17 14:41
+ *  * @since
+ *  
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ESApplication {
+}
